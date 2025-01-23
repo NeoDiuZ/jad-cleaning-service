2ca16a8c8139dabe8e91fc57aa27d9dc89d6e05d
Arkar <arkarphyo@gmail.com>
2025-01-23 09:43:26 +0800

    Intial file structure

---
+++ src/test/java/sp/dit/jad/cleaning_service/CleaningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sp.dit.jad.cleaning_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleaningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
